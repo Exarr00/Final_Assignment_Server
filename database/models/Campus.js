c4940732ac58fa5aafe39de667420a6ecf04e7bc
Andy N <Andy.Ni87@myhunter.cuny.edu>
2021-12-15 09:35:43 -0500

    campus model

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -8,8 +8,18 @@ const Campus = db.define("campus", {
     allowNull: false
   },
 
-  description: {
+  imageUrl: {
     type: Sequelize.STRING,
+    defaultValue: "https://www.smith.edu/sites/default/files/media/Documents/College-Relations/gates-photographypage.jpg"
+  },
+
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  description: {
+    type: Sequelize.TEXT,
   }
 
 });
