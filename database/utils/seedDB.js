7af0bff97c75c6b1bd07207de9cf926f20c3769d
Andy N <Andy.Ni87@myhunter.cuny.edu>
2021-12-15 09:40:16 -0500

    added more dummy

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -3,19 +3,31 @@ const { Campus, Student } = require('../models');
 const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
+		address: "Check google maps",
 		description: "This is a school in NYC."
 	});
 	const dummy_campus2 = await Campus.create({
 		name: "Harvard",
+		address: "Check google maps",
 		description: "This is a school in MA."
 	});
 
 	const dummy_student = await Student.create({
 			firstname: "Joe",
-      lastname: "Shmo",
+      		lastname: "Shmo",
+			email: "wow@gmail.com",
+			gpa: 3.9
+		});
+
+	const dummy_student2 = await Student.create({
+			firstname: "this",
+      		lastname: "person",
+			email: "someone@gmail.com",
+			gpa: 4.0
 		});
 
 	await dummy_student.setCampus(dummy_campus);
+	await dummy_student2.setCampus(dummy_campus2);
 	
 }
 
